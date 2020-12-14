40d6bbfdda00b97695cd13459d19863609404247
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-14 16:09:51 -0700

    Add index, show, create,update, and destroy actions

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,3 +1,33 @@
 class Api::V1::ItemsController < ApplicationController
-  def index; end
+  def index
+    items = Item.all
+    render json: ItemSerializer.new(items).serializable_hash.to_json
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: ItemSerializer.new(item).serializable_hash.to_json
+  end
+
+  def create
+    item = Item.create!(item_params)
+    render json: ItemSerializer.new(item).serializable_hash.to_json
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    render json: ItemSerializer.new(item).serializable_hash.to_json
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
