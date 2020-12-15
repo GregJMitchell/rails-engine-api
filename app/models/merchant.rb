02058a2cad3323b9de007142007cc854162d587e
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-14 20:47:28 -0700

    Create merchant scopes to search

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,7 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
 
   has_many :items
+
+  scope :search_all, ->(search) { where('name ILIKE :search', search: "%#{search.downcase}%") }
+  scope :search_one, ->(search) { find_by('name ILIKE :search', search: "%#{search.downcase}%") }
 end
