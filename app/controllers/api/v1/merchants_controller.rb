b0a36c624fb4d02393328c071248c74d146f833a
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-14 19:00:23 -0700

    Add action for deleteing a merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::MerchantsController < ApplicationController
     merchants = Merchant.all
     render json: MerchantSerializer.new(merchants).serializable_hash.to_json
   end
-  
+
   def show
     merchant = Merchant.find(params[:id])
     render json: MerchantSerializer.new(merchant).serializable_hash.to_json
@@ -14,7 +14,13 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(merchant).serializable_hash.to_json
   end
 
+  def destroy
+    merchant = Merchant.find(params[:id])
+    merchant.destroy
+  end
+
   private
+
   def merchant_params
     params.permit(:name)
   end
