5ef3653961a326f348444398c3989a513b251982
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-16 20:32:13 -0700

    Move data creation to before :each and add test for most items endpoint

--- spec/requests/api/v1/business_intel_requests_spec.rb
+++ spec/requests/api/v1/business_intel_requests_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 describe 'Business intelligence endpoints' do
-  it 'can get merchants with most revenue' do
-    merchant1 = create(:merchant, name: 'merchant1')
-    merchant2 = create(:merchant , name: 'merchant2')
-    merchant3 = create(:merchant)
-    merchant4 = create(:merchant, name: 'merchant4')
-    merchant5 = create(:merchant, name: 'merchant5')
-    merchant6 = create(:merchant)
-    merchant7 = create(:merchant)
-    merchant8 = create(:merchant)
-    merchant9 = create(:merchant, name: 'merchant9')
-    merchant10 = create(:merchant)
+  before :each do
+    @merchant1 = create(:merchant, name: 'merchant1')
+    @merchant2 = create(:merchant, name: 'merchant2')
+    @merchant3 = create(:merchant)
+    @merchant4 = create(:merchant, name: 'merchant4')
+    @merchant5 = create(:merchant, name: 'merchant5')
+    @merchant6 = create(:merchant)
+    @merchant7 = create(:merchant)
+    @merchant8 = create(:merchant)
+    @merchant9 = create(:merchant, name: 'merchant9')
+    @merchant10 = create(:merchant)
 
     10.times do
-      invoices = create_list(:invoice, 10, merchant: merchant1)
+      invoices = create_list(:invoice, 10, merchant: @merchant1)
       invoices.each do |invoice|
         create(:invoice_item, invoice: invoice, quantity: 200, unit_price: 1000)
         create(:transaction, invoice: invoice)
       end
     end
     7.times do
-      invoices = create_list(:invoice, 5, merchant: merchant5)
+      invoices = create_list(:invoice, 5, merchant: @merchant5)
       invoices.each do |invoice|
         create(:invoice_item, invoice: invoice, quantity: 100, unit_price: 1000)
         create(:transaction, invoice: invoice)
@@ -29,40 +29,60 @@ describe 'Business intelligence endpoints' do
     end
 
     4.times do
-      invoices = create_list(:invoice, 5, merchant: merchant9)
+      invoices = create_list(:invoice, 5, merchant: @merchant9)
       invoices.each do |invoice|
         create(:invoice_item, invoice: invoice, quantity: 50, unit_price: 1000)
         create(:transaction, invoice: invoice)
       end
     end
 
-    invoices = create_list(:invoice, 2, merchant: merchant2)
+    invoices = create_list(:invoice, 2, merchant: @merchant2)
     invoices.each do |invoice|
       create(:invoice_item, invoice: invoice, quantity: 25, unit_price: 1000)
       create(:transaction, invoice: invoice)
     end
 
-    invoices = create_list(:invoice, 2, merchant: merchant4)
+    invoices = create_list(:invoice, 2, merchant: @merchant4)
     invoices.each do |invoice|
       create(:invoice_item, invoice: invoice, quantity: 5, unit_price: 1000)
       create(:transaction, invoice: invoice)
     end
+  end
 
+  it 'can get merchants with most revenue' do
     get '/api/v1/merchants/most_revenue?quantity=5'
     json = JSON.parse(response.body, symbolize_names: true)
 
-    
     expect(response).to be_successful
 
     expect(json[:data].length).to eq(5)
 
-    expect(json[:data][0][:attributes][:name]).to eq(merchant1.name)
-    expect(json[:data][0][:id]).to eq(merchant1.id.to_s)
+    expect(json[:data][0][:attributes][:name]).to eq(@merchant1.name)
+    expect(json[:data][0][:id]).to eq(@merchant1.id.to_s)
 
-    expect(json[:data][1][:attributes][:name]).to eq(merchant5.name)
-    expect(json[:data][1][:id]).to eq(merchant5.id.to_s)
+    expect(json[:data][1][:attributes][:name]).to eq(@merchant5.name)
+    expect(json[:data][1][:id]).to eq(@merchant5.id.to_s)
 
-    expect(json[:data][2][:attributes][:name]).to eq(merchant9.name)
-    expect(json[:data][2][:id]).to eq(merchant9.id.to_s)
+    expect(json[:data][2][:attributes][:name]).to eq(@merchant9.name)
+    expect(json[:data][2][:id]).to eq(@merchant9.id.to_s)
+  end
+
+  it 'can get merchants who have sold the most items' do
+    get '/api/v1/merchants/most_items?quantity=3'
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(json[:data].length).to eq(3)
+
+    expect(json[:data][0][:attributes][:name]).to eq(@merchant1.name)
+    expect(json[:data][0][:id]).to eq(@merchant1.id.to_s)
+
+    expect(json[:data][1][:attributes][:name]).to eq(@merchant5.name)
+    expect(json[:data][1][:id]).to eq(@merchant5.id.to_s)
+
+    expect(json[:data][2][:attributes][:name]).to eq(@merchant9.name)
+    expect(json[:data][2][:id]).to eq(@merchant9.id.to_s)
   end
 end
