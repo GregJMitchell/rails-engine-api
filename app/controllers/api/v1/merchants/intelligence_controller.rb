35e29138266feda1888f5823aabae73050dd59fd
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-16 20:32:41 -0700

    Create action for most items endpoint

--- app/controllers/api/v1/merchants/intelligence_controller.rb
+++ app/controllers/api/v1/merchants/intelligence_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::Merchants::IntelligenceController < ApplicationController
     merchants = Merchant.most_revenue(params[:quantity])
     render json: MerchantSerializer.new(merchants).serializable_hash.to_json
   end
+
+  def most_items
+    merchants = Merchant.most_items(params[:quantity])
+    render json: MerchantSerializer.new(merchants).serializable_hash.to_json
+  end
 end
