a107076802dbc9798904a9495db04141681655c6
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-16 20:21:59 -0700

    Create most reveue query method and add relationships

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,20 @@ class Merchant < ApplicationRecord
 
   has_many :items
 
+  has_many :invoices
+
+  has_many :invoice_items, through: :invoices
+
   scope :search_all, ->(search) { where('name ILIKE :search', search: "%#{search.downcase}%") }
   scope :search_one, ->(search) { find_by('name ILIKE :search', search: "%#{search.downcase}%") }
+
+  def self.most_revenue(quantity)
+    Merchant.joins(invoices: %i[transactions invoice_items])
+            .where(transactions: { result: 'success' })
+            .where(invoices: { status: 'shipped' })
+            .select('merchants.*, sum(invoice_items.quantity*invoice_items.unit_price) as revenue')
+            .group('merchants.id')
+            .order('revenue desc')
+            .limit(quantity.to_i)
+  end
 end
