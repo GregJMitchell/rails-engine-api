41014e0c9d1e0a60507ee12c49d1366c88d38a8d
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-17 21:34:01 -0700

    Create revenue actions

--- app/controllers/api/v1/merchants/intelligence_controller.rb
+++ app/controllers/api/v1/merchants/intelligence_controller.rb
@@ -8,4 +8,15 @@ class Api::V1::Merchants::IntelligenceController < ApplicationController
     merchants = Merchant.most_items(params[:quantity])
     render json: MerchantSerializer.new(merchants).serializable_hash.to_json
   end
+
+  def revenue_between_dates
+    revenue = Invoice.revenue(params[:start], params[:end])[0]
+    render json: RevenueSerializer.new(revenue).serializable_hash.to_json
+  end
+
+  def revenue
+    merchant = Merchant.find(params[:id])
+    revenue = merchant.revenue
+    render json: RevenueSerializer.new(revenue[0]).serializable_hash.to_json
+  end
 end
